e6efc708dcec8788ab16e8b63b61b86cdab85ac9
verde-mar <sara.grecu25@gmail.com>
2022-03-28 10:47:52 +0200

    fixes

--- script/secondo_test.sh
+++ script/secondo_test.sh
@@ -16,7 +16,7 @@ pid=$!
 pidcl1=$!
 ./cl -f socket -p -t 125 -D ./flushed -W ./test_dir1/prova3.txt -D ./flushed -W ./test_dir1/prova4.txt -D ./flushed -W ./test_dir2/sea2.jpg &
 pidcl2=$!
-./cl -f socket -p -t 100 -D ./flushed -W ./test_dir1/prova5.txt -D ./flushed -W ./test_dir1/prova11.txt -D ./flushed -W ./test_dir1/prova11.txt &
+./cl -f socket -p -t 100 -D ./flushed -W ./test_dir1/prova5.txt -D ./flushed -W ./test_dir1/prova11.txt &
 pidcl3=$!
 ./cl -f socket -p -t 125 -D ./flushed -W ./test_dir2/prova12.txt -D ./flushed -W ./test_dir2/sea1.jpg  &
 pidcl4=$!
