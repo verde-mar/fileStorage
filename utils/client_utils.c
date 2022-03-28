1d6fc09cfa0723d21383869a1807d3b1c16e0f9a
verde-mar <sara.grecu25@gmail.com>
2022-03-28 12:37:01 +0200

    last fixes

--- utils/client_utils.c
+++ utils/client_utils.c
@@ -187,6 +187,7 @@ int receiver(int *byte_letti, int *byte_scritti, size_t size_path, char** path, 
     *byte_letti += read_msg(fd_skt, *path, size_path);
     CHECK_OPERATION(errno == EFAULT,
         fprintf(stderr, "Non e' stato possibile leggere il buffer che sta per arrivare e che sta per essere memorizzato.\n"); 
+        free(path);
         return -1);
 
     /* Legge la size del buffer di dati che si sta per ricevere */    
@@ -194,14 +195,15 @@ int receiver(int *byte_letti, int *byte_scritti, size_t size_path, char** path, 
     *byte_letti += read_size(fd_skt, size_old); 
     CHECK_OPERATION(errno == EFAULT,
         fprintf(stderr, "Non e' stato possibile leggere la size del file che stavo per memorizzare.\n"); 
-                return -1);
-    printf("SIZE FILE IN RECEIVER: %ld\n", *size_old);
+        free(path);    
+        return -1);
     
     if(*size_old > 0){
         *old_file = malloc(*size_old);
         CHECK_OPERATION(*old_file == NULL,
             fprintf(stderr, "Allocazione non andata a buon fine.\n");
-                return -1);
+            free(path);
+            return -1);
             
 
         /* Legge il buffe di dati che si sta per ricevere */
@@ -209,9 +211,11 @@ int receiver(int *byte_letti, int *byte_scritti, size_t size_path, char** path, 
         *byte_letti += read_msg(fd_skt, *old_file, *size_old);
         CHECK_OPERATION(errno == EFAULT,
             fprintf(stderr, "Non e' stato possibile leggere il file.\n"); 
-                    return -1);
+                free(path);
+                free(old_file);
+                return -1);
     } else {
-        free(path);
+        if(path)free(path);
         return -2;
     }
                 
