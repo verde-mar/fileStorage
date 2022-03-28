952679602214c1d226a88544068bb62b9ec0b127
verde-mar <sara.grecu25@gmail.com>
2022-03-28 12:43:37 +0200

    last

--- script/secondo_test.sh
+++ script/secondo_test.sh
@@ -12,13 +12,13 @@ sleep 2
 pid=$!
 
 # Avvia i client
-./cl -f socket -p -t 100 -D ./flushed -W ./test_dir1/sea.jpg -D ./flushed -W ./test_dir1/prova.txt -D ./flushed -W ./test_dir1/prova2.txt  &
+valgrind --leak-check=full  ./cl -f socket -p -t 100 -D ./flushed -W ./test_dir1/sea.jpg -D ./flushed -W ./test_dir1/prova.txt -D ./flushed -W ./test_dir1/prova2.txt  &
 pidcl1=$!
-./cl -f socket -p -t 125 -D ./flushed -W ./test_dir1/prova3.txt -D ./flushed -W ./test_dir1/prova4.txt -D ./flushed -W ./test_dir2/sea2.jpg &
+valgrind --leak-check=full ./cl -f socket -p -t 125 -D ./flushed -W ./test_dir1/prova3.txt -D ./flushed -W ./test_dir1/prova4.txt -D ./flushed -W ./test_dir2/sea2.jpg &
 pidcl2=$!
-./cl -f socket -p -t 100 -D ./flushed -W ./test_dir1/prova5.txt -D ./flushed -W ./test_dir1/prova11.txt &
+valgrind --leak-check=full ./cl -f socket -p -t 100 -D ./flushed -W ./test_dir1/prova5.txt -D ./flushed -W ./test_dir1/prova11.txt &
 pidcl3=$!
-./cl -f socket -p -t 125 -D ./flushed -W ./test_dir2/prova12.txt -D ./flushed -W ./test_dir2/sea1.jpg  &
+valgrind --leak-check=full ./cl -f socket -p -t 125 -D ./flushed -W ./test_dir2/prova12.txt -D ./flushed -W ./test_dir2/sea1.jpg  &
 pidcl4=$!
 
 wait $pidcl1 $pidcl2 $pidcl3 $pidcl4
